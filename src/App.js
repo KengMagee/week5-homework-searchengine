556751b43a6b084755c70676fd665c624d5a1cc9
KengMagee <155960435+KengMagee@users.noreply.github.com>
2024-05-12 13:02:54 +1000

    added Github link and SheCodes link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        This project was coded by
+        <a
+          href="https://www.shecodes.io/graduates/102970-karen-felipe"
+          target="_blank"
+        >
+          Karen Felipe
+        </a>{" "}
+        and is open-sourced on
+        <a
+          href="https://github.com/KengMagee/week5-homework-searchengine"
+          target="_blank"
+        >
+          Github
+        </a>
+      </p>
+    </div>
+  );
 }
