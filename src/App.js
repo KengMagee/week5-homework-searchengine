c8582a7b714f8fd5ce5372dde129738e566ea133
KengMagee <155960435+KengMagee@users.noreply.github.com>
2024-05-18 21:18:11 +1000

    ran npm update command and added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <p>
-        This project was coded by
-        <a
-          href="https://www.shecodes.io/graduates/102970-karen-felipe"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Karen Felipe
-        </a>{" "}
-        and is open-sourced on
-        <a
-          href="https://github.com/KengMagee/week5-homework-searchengine"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather App</h1>
+        <button className="btn btn-primary">Hello</button>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/102970-karen-felipe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Karen Felipe
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/KengMagee/week5-homework-searchengine"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
